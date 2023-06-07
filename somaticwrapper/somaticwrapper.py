7158e105689307d0a9b390e0a511fde3c769ee04
Erik Storrs <epstorrs@gmail.com>
2023-06-07 10:42:52 -0500

    change cwd

--- somaticwrapper/somaticwrapper.py
+++ somaticwrapper/somaticwrapper.py
@@ -29,19 +29,22 @@ parser.add_argument('--reference', type=str,
 parser.add_argument('--rescue-genes', type=str, required=True,
     help='Which genes to rescue from vaf filter. Either a .bed file with 4 columns: <chrom> <start> <stop> <gene>. Or a .txt file with one gene per line.')
 
+parser.add_argument('--script-dir', type=str, default='/pecgs-somaticwrapper/somaticwrapper',
+    help='Directory with all the somaticwrapper perl scripts')
+
 args = parser.parse_args()
 
 
 SMG_FP = 'smg.txt'
 
 
-def call_somaticwrapper(step):
+def call_somaticwrapper(run_dir, smg_fp, log_fp, step):
     pieces = [
         'perl somaticwrapper.pl',
-        '-rdir', args.run_dir,
-        '--log', 'log_step{step}.txt',
+        '-rdir', run_dir,
+        '--log', log_fp,
         '--ref', args.reference,
-        '--smg', SMG_FP,
+        '--smg', smg_fp,
         '--step', str(step)
     ]
     return ' '.join(pieces)
@@ -68,9 +71,19 @@ def run_somaticwrapper():
     logging.info('setting up run directory')
     setup_run()
 
+    run_dir = os.path.abspath(args.run_dir)
+    smg_fp = os.path.abspath(SMG_FP)
+    log_fp = os.path.join(os.path.abspath(args.run_dir), f'step{i}.log')
+
+    logging.info(f'run dir: {run_dir}')
+    logging.info(f'smg filepath: {smg_fp}')
+    logging.info(f'log filepath: {log_fp}')
+
+    os.chdir(args.script_dir)
+
     for i in range(1, 15, 1):
         logging.info(f'running step {i}')
-        cmd = call_somaticwrapper(i)
+        cmd = call_somaticwrapper(run_dir, smg_fp, log_fp, i)
         logging.info(f'executing command: {cmd}')
         subprocess.check_output(cmd, shell=True)
 
